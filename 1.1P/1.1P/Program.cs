0b265648d7f182b60bd3cbee167fb2c667f546a7
NathanTrung <49798796+NathanTrung@users.noreply.github.com>
2022-09-04 15:19:19 +1000

    Add files via upload

---
+++ 1.1P/1.1P/Program.cs
@@ -0,0 +1,13 @@
+﻿public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        MainClass main = new MainClass();
+        main.TestMethod("Wilma");
+    }
+
+    public void TestMethod(string a)
+    {
+        Console.WriteLine(a + "Smith");
+    }
+}
